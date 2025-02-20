cd70c05dd8aa0bf0be036baeec73e806a2fa4445
FrancooChaparro <francoo_chaparro@hotmail.com>
2025-02-20 16:26:29 -0300

    fix

--- app/(routes)/team/[slug]/page.tsx
+++ app/(routes)/team/[slug]/page.tsx
@@ -2,18 +2,16 @@ import { Suspense } from "react";
 import UserPage from "./page.client";
 import { find_team_slug } from " @/utils/get_team_slug";
 import Loader from " @/components/Loader/Loader";
-
-
+import NotFound from " @/app/not-found";
 
 export default async function ServerPage({ params } : { params: { slug: string }}) {
 
   const data =  await find_team_slug(params.slug);
 
   if (!data) {
-    return <div>Equipo no encontrado</div>;
+    return NotFound();
   }
 
-  
   return (
     <Suspense key={null} fallback={<Loader />}>
         <UserPage data={data}/>
